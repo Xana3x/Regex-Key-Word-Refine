98e75e992b1736fe386f192de13db189916c5bc4
WenGE <u202214192@hust.edu.cn>
2024-11-05 09:37:43 -0500

    added a prompt template

--- prompt.py
+++ prompt.py
@@ -12,6 +12,20 @@ Please finish this task step by step.
 The output should be a new list wrapped by ```KeyWordsList```.
 """
 
+def zero_shot_prompt_template_with_phrase_list(cwe_description,key_words_list,phrase_list):
+    return f"""Please read this CWE descrption:
+```
+{cwe_description}
+```
+Based on this description and the list of phrases provided to you below, please select some words from the keyword list provided below, combine the words in the keyword list with existing phrases, enrich the content of the keyword list, and generate phrase keywords that are more representative of the CWE description. At the same time, please consider the parts of speech and tenses of each word, and combine them into more diverse phrases.
+Here is the list of keywords:
+```
+{key_words_list}
+```
+Please finish this task step by step. 
+The output should be a new list wrapped by ```KeyWordsList```.
+"""
+
 
 def few_shot_prompt_template(cwe_description,examples,key_words_list):
     return f"""Please read this CWE descrption:
